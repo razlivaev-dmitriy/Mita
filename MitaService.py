5f04590e19718aeb49a786bf4b9961cbfc03a7ec
razlivaev-dmitriy <142737939+razlivaev-dmitriy@users.noreply.github.com>
2026-02-12 12:39:41 +0300

    Update MitaService.py

--- MitaService.py
+++ MitaService.py
@@ -55,13 +55,11 @@ def install_service():
             exeName=sys.executable,
             exeArgs=f'"{script_path}"'
         )
-        input()
         print("Сервис установлен успешно")
         return True
         
     except Exception as e:
         print(f"Ошибка установки: {e}")
-        input()
         return False
     
 class LearnFiles():
@@ -435,9 +433,7 @@ if __name__ == '__main__':
                 print("Сервис запускается...")
                 import win32serviceutil
                 win32serviceutil.StartService("MitaDataCollectionService")
-                input()
                 print("Сервис запущен")
-            input()
         elif sys.argv[1] == "debug":
             print("Сервис запускается...")
             service = MitaDataCollectionService(["MitaDataCollectionService", "debug"])
@@ -464,5 +460,6 @@ if __name__ == '__main__':
         else:
             import win32serviceutil
             win32serviceutil.HandleCommandLine(MitaDataCollectionService)
+
 
 
